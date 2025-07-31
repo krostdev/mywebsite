33a951eb427fb5a0df665c54f8df76b3e23f4a13
krostdev <augustokauanpe@gmail.com>
2025-07-31 11:39:34 -0300

    Notificação de desenvolvimento

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import Header from './components/Header/App';
 import Skills from './components/Skills/App';
 
+import { toast, ToastContainer } from 'react-toastify';
+import { useEffect } from 'react';
+
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    toast("Este site está em desenvolvimento", {
+        hideProgressBar: true,
+        closeButton: false,
+        position: "bottom-right",
+        style: {
+            color: "#000",
+            marginBottom: "7px",
+            fontSize: "18px"
+        }
+    })
+  },[])
 
   return (
     <>
+    <ToastContainer/>
     <Header/>
     <Skills/>
     </>
